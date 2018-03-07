3a3edc9d356c73b4d48d5b80333e37462fbdc7a2
indianstau <indianstau@yahoo.com.tw>
2018-03-07 17:26:38 +0800

    mid-roll

---
+++ IOS/ImaVastTest/ViewController.m
@@ -0,0 +1,326 @@
+//
+//  ViewController.m
+//  ImaVastTest
+//
+//  Created by San on 2018/3/2.
+//  Copyright © 2018年 Brightcove. All rights reserved.
+//
+
+@import GoogleInteractiveMediaAds;
+
+@import BrightcovePlayerSDK;
+@import BrightcoveIMA;
+
+#import "ViewController.h"
+
+static NSString * const kViewControllerPlaybackServicePolicyKey = @"BCpkADawqM0ZXsbRNOVVi-pMCK4K5iIZEBZQyVq5nV8sp8m47ua6r84ibaEnNa6tmFNcmuyb3D7Xl9h_NNBImikzUvHPZCBxO5BOlwKA9MTyBPfvLItNLgIYm27VagaeymPshjcQTV7lJgoY";
+static NSString * const kViewControllerAccountID = @"4005328949001";
+static NSString * const kViewControllerVideoID = @"5714150331001";
+
+static NSString * const kViewControllerIMAPublisherID = @"insertyourpidhere";
+static NSString * const kViewControllerIMALanguage = @"en";
+//static NSString * const kVIEWControllerIMAVASTResponseAdTag = @"https://pubads.g.doubleclick.net/gampad/ads?sz=1280x720&iu=/18689016/setn_video_pre-roll_1280x720_2&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]";
+
+static NSString * const kVIEWControllerIMAVASTResponseAdTag = @"https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=";
+
+
+
+@interface ViewController () <BCOVPlaybackControllerDelegate>
+
+@property (nonatomic, strong) BCOVPlaybackService *playbackService;
+@property (nonatomic, strong) id<BCOVPlaybackController> playbackController;
+@property (nonatomic) BCOVPUIPlayerView *playerView;
+@property (nonatomic, weak) IBOutlet UIView *videoContainer;
+
+@property (nonatomic, assign) BOOL adIsPlaying;
+@property (nonatomic, assign) BOOL isBrowserOpen;
+@property (nonatomic, strong) id<NSObject> notificationReceipt;
+
+@end
+
+@implementation ViewController
+
+#pragma mark Setup Methods
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:_notificationReceipt];
+}
+
+
+//- (instancetype)initWithCoder:(NSCoder *)coder
+//{
+//    self = [super initWithCoder:coder];
+//    if (self)
+//    {
+//        [self setup];
+//    }
+//    return self;
+//}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    // Set up our player view. Create with a standard VOD layout.
+    //BCOVPUIPlayerView *playerView = [[BCOVPUIPlayerView alloc] initWithPlaybackController:self.playbackController options:nil controlsView:[BCOVPUIBasicControlView basicControlViewWithVODLayout] ];
+    
+    // Install in the container view and match its size.
+    //playerView.frame = _videoContainer.bounds;
+    //playerView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    //[_videoContainer addSubview:playerView];
+    //_playerView = playerView;
+    
+    // Associate the playerView with the playback controller.
+    //_playerView.playbackController = _playbackController;
+    
+    [self setup];
+    
+    [self requestContentFromPlaybackService];
+    
+}
+
+
+- (void)createPlayerView
+{
+    if (!self.playerView)
+    {
+        BCOVPUIPlayerViewOptions *options = [[BCOVPUIPlayerViewOptions alloc] init];
+        options.presentingViewController = self;
+        
+        BCOVPUIBasicControlView *controlView = [BCOVPUIBasicControlView basicControlViewWithVODLayout];
+        // Set playback controller later.
+        self.playerView = [[BCOVPUIPlayerView alloc] initWithPlaybackController:nil options:options controlsView:controlView];
+        self.playerView.frame = self.videoContainer.bounds;
+        self.playerView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        [self.videoContainer addSubview:self.playerView];
+    }
+    else
+    {
+        NSLog(@"PlayerView already exists");
+    }
+}
+
+
+- (void)setup
+{
+    //_playbackController = [BCOVPlayerSDKManager.sharedManager createPlaybackController];
+    
+    //_playbackController.analytics.account = kViewControllerAccountID; // Optional
+    
+    //_playbackController.delegate = self;
+    //_playbackController.autoAdvance = YES;
+    //_playbackController.autoPlay = YES;
+    
+    //_playbackService = [[BCOVPlaybackService alloc] initWithAccountId:kViewControllerAccountID policyKey:kViewControllerPlaybackServicePolicyKey];
+    [self createPlayerView];
+    
+    BCOVPlayerSDKManager *manager = [BCOVPlayerSDKManager sharedManager];
+    
+    IMASettings *imaSettings = [[IMASettings alloc] init];
+    imaSettings.ppid = kViewControllerIMAPublisherID;
+    imaSettings.language = kViewControllerIMALanguage;
+    
+    IMAAdsRenderingSettings *renderSettings = [[IMAAdsRenderingSettings alloc] init];
+    renderSettings.webOpenerPresentingController = self;
+    renderSettings.webOpenerDelegate = self;
+    
+    // BCOVIMAAdsRequestPolicy provides methods to specify VAST or VMAP/Server Side Ad Rules. Select the appropriate method to select your ads policy.
+    
+   
+    
+//    BCOVCuePointProgressPolicy *policy = [BCOVCuePointProgressPolicy                          progressPolicyProcessingCuePoints:BCOVProgressPolicyProcessFinalCuePoint resumingPlaybackFrom:BCOVProgressPolicyResumeFromContentPlayhead ignoringPreviouslyProcessedCuePoints:NO];
+    
+   BCOVIMAAdsRequestPolicy * adsRequestPolicy = [BCOVIMAAdsRequestPolicy adsRequestPolicyFromCuePointPropertiesWithAdTag:kVIEWControllerIMAVASTResponseAdTag adsCuePointProgressPolicy:nil];
+    
+//    BCOVIMAAdsRequestPolicy *adsRequestPolicy = [BCOVIMAAdsRequestPolicy adsRequestPolicyWithVASTAdTagsInCuePointsAndAdsCuePointProgressPolicy:policy];
+    
+    self.playbackController = [manager createIMAPlaybackControllerWithSettings:imaSettings
+                                                          adsRenderingSettings:renderSettings
+                                                              adsRequestPolicy:adsRequestPolicy
+                                                                   adContainer:self.playerView.contentOverlayView
+                                                                companionSlots:nil
+                                                                  viewStrategy:nil];
+    self.playbackController.delegate = self;
+    self.playbackController.autoAdvance = YES;
+    self.playbackController.autoPlay = YES;
+    
+    self.playerView.playbackController = self.playbackController;
+    
+    // Creating a playback controller based on the above code will create
+    // VMAP / Server Side Ad Rules. These settings are explained in BCOVIMAAdsRequestPolicy.h.
+    // If you want to change these settings, you can initialize the plugin like so:
+    //
+    // or for VAST:
+    //
+
+    //
+    // _playbackController = [manager createIMAPlaybackControllerWithSettings:imaSettings
+    //                                                   adsRenderingSettings:renderSettings
+    //                                                       adsRequestPolicy:adsRequestPolicy
+    //                                                            adContainer:self.playerView.contentOverlayView
+    //                                                         companionSlots:nil
+    //                                                           viewStrategy:nil];
+    //
+    
+    
+    self.playbackService = [[BCOVPlaybackService alloc] initWithAccountId:kViewControllerAccountID
+                                                                policyKey:kViewControllerPlaybackServicePolicyKey];
+    
+    [self resumeAdAfterForeground];
+}
+
+
+- (void)resumeAdAfterForeground
+{
+    // When the app goes to the background, the Google IMA library will pause
+    // the ad. This code demonstrates how you would resume the ad when entering
+    // the foreground.
+
+    ViewController * __weak weakSelf = self;
+
+    self.notificationReceipt = [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationWillEnterForegroundNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+
+        ViewController *strongSelf = weakSelf;
+
+        if (strongSelf.adIsPlaying && !strongSelf.isBrowserOpen)
+        {
+            [strongSelf.playbackController resumeAd];
+        }
+
+    }];
+}
+
+//+ (BCOVVideo *)updateVideo:(BCOVVideo *)video
+//{
+//    // Update each video to add the tag.
+//    return [video update:^(id<BCOVMutableVideo> mutableVideo) {
+//
+//        // The BCOVIMA plugin will look for the presence of kBCOVIMAAdTag in
+//        // the video's properties when using server side ad rules. This URL returns
+//        // a VMAP response that is handled by the Google IMA library.
+//        NSDictionary *adProperties = @{ kBCOVIMAAdTag : kVIEWControllerIMAVASTResponseAdTag };
+//
+//        NSMutableDictionary *propertiesToUpdate = [mutableVideo.properties mutableCopy];
+//        [propertiesToUpdate addEntriesFromDictionary:adProperties];
+//        mutableVideo.properties = propertiesToUpdate;
+//
+//    }];
+//}
+
+
+- (void)requestContentFromPlaybackService
+{
+    [self.playbackService findVideoWithVideoID:kViewControllerVideoID parameters:nil completion:^(BCOVVideo *video, NSDictionary *jsonResponse, NSError *error) {
+        
+        if (video)
+        {
+//            BCOVVideo *updatedVideo = [video update:^(id<BCOVMutableVideo> mutableVideo) {
+//
+//                mutableVideo.cuePoints = [[BCOVCuePointCollection alloc] initWithArray:@[
+//             [[BCOVCuePoint alloc] initWithType:kBCOVIMACuePointTypeAd
+//                                       position:CMTimeMake(10,1)
+//                                     properties:@{ kBCOVIMAAdTag : @"https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=" }],
+//             ]];
+//            }];
+            // Get the video duration from the properties dictionary
+//            NSNumber *durationNumber = video.properties[@"duration"]; // milliseconds
+//            float duration = durationNumber.floatValue / 1000.0; // convert to seconds
+//
+//            video = [video update:^(id<BCOVMutableVideo> mutableVideo)
+//            {
+//                 // Add quarterly interval cue points of your own type
+//                 BCOVCuePoint *cp1 = [[BCOVCuePoint alloc] initWithType:@"ad" position:CMTimeMake(duration * 250, 1000)];
+//                 BCOVCuePoint *cp2 = [[BCOVCuePoint alloc] initWithType:@"ad" position:CMTimeMake(duration * 500, 1000)];
+//                 BCOVCuePoint *cp3 = [[BCOVCuePoint alloc] initWithType:@"ad" position:CMTimeMake(duration * 750, 1000)];
+//                 BCOVCuePoint *cp4 = [[BCOVCuePoint alloc] initWithType:@"ad" position:CMTimeMake(duration * 1000, 1000)];
+//
+//                 // Create new cue point collection using existing cue points and new cue points
+//                 NSMutableArray *newCuePoints = [[NSMutableArray alloc] initWithArray:mutableVideo.cuePoints.array];
+//                 [newCuePoints addObject:cp1];
+//                 [newCuePoints addObject:cp2];
+//                 [newCuePoints addObject:cp3];
+//                 [newCuePoints addObject:cp4];
+//
+//                 mutableVideo.cuePoints = [[BCOVCuePointCollection alloc] initWithArray:newCuePoints];
+//            }];
+            [self.playbackController setVideos:@[ video ]];
+//            [self.playbackController setVideos:@[ updatedVideo ]];
+        }
+        else
+        {
+            NSLog(@"ViewController Debug - Error retrieving video: %@", error);
+        }
+        
+    }];
+}
+
+#pragma mark BCOVPlaybackControllerDelegate Methods
+
+- (void)playbackController:(id<BCOVPlaybackController>)controller didAdvanceToPlaybackSession:(id<BCOVPlaybackSession>)session
+{
+    NSLog(@"ViewController Debug - Advanced to new session.");
+}
+
+-(void)playbackController:(id<BCOVPlaybackController>)controller playbackSession:(id<BCOVPlaybackSession>)session didReceiveLifecycleEvent:(BCOVPlaybackSessionLifecycleEvent *)lifecycleEvent
+{
+    // Ad events are emitted by the BCOVIMA plugin through lifecycle events.
+    // The events are defined BCOVIMAComponent.h.
+    
+    NSString *type = lifecycleEvent.eventType;
+    
+    if ([type isEqualToString:kBCOVIMALifecycleEventAdsLoaderLoaded])
+    {
+        NSLog(@"ViewController Debug - Ads loaded.");
+    }
+    else if ([type isEqualToString:kBCOVIMALifecycleEventAdsManagerDidReceiveAdEvent])
+    {
+        IMAAdEvent *adEvent = lifecycleEvent.properties[@"adEvent"];
+        
+        switch (adEvent.type)
+        {
+            case kIMAAdEvent_STARTED:
+                NSLog(@"ViewController Debug - Ad Started.");
+                self.adIsPlaying = YES;
+                break;
+            case kIMAAdEvent_COMPLETE:
+                NSLog(@"ViewController Debug - Ad Completed.");
+                self.adIsPlaying = NO;
+                break;
+            case kIMAAdEvent_ALL_ADS_COMPLETED:
+                NSLog(@"ViewController Debug - All ads completed.");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+- (void)playbackController:(id<BCOVPlaybackController>)controller playbackSession:(id<BCOVPlaybackSession>)session didEnterAdSequence:(BCOVAdSequence *)adSequence
+{
+    // Hide all controls for ads (so they're not visible when full-screen)
+    self.playerView.controlsContainerView.alpha = 0.0;
+}
+
+- (void)playbackController:(id<BCOVPlaybackController>)controller playbackSession:(id<BCOVPlaybackSession>)session didExitAdSequence:(BCOVAdSequence *)adSequence
+{
+    // Show all controls when ads are finished.
+    self.playerView.controlsContainerView.alpha = 1.0;
+}
+
+#pragma mark IMAWebOpenerDelegate Methods
+
+- (void)webOpenerDidCloseInAppBrowser:(NSObject *)webOpener
+{
+    // Called when the in-app browser has closed.
+    [self.playbackController resumeAd];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

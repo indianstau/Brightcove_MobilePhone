da715855fc68eb0a0fa47d2d8f4ed604d2ddf315
indianstau <indianstau@yahoo.com.tw>
2018-03-08 09:58:22 +0800

    MidRollAd

---
+++ Android/MidrollCuePoints/MainActivity.java
@@ -0,0 +1,177 @@
+package com.brightcove.midrollcuepoints;
+
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.brightcove.ima.GoogleIMAComponent;
+import com.brightcove.ima.GoogleIMAEventType;
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.PlaylistListener;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.Event;
+import com.brightcove.player.event.EventEmitter;
+import com.brightcove.player.event.EventListener;
+import com.brightcove.player.event.EventType;
+import com.brightcove.player.media.DeliveryType;
+import com.brightcove.player.media.VideoFields;
+import com.brightcove.player.mediacontroller.BrightcoveMediaController;
+import com.brightcove.player.model.CuePoint;
+import com.brightcove.player.model.Playlist;
+import com.brightcove.player.model.Source;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.util.StringUtil;
+import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.brightcove.player.view.BrightcovePlayer;
+import com.google.ads.interactivemedia.v3.api.AdDisplayContainer;
+import com.google.ads.interactivemedia.v3.api.AdsRequest;
+import com.google.ads.interactivemedia.v3.api.CompanionAdSlot;
+import com.google.ads.interactivemedia.v3.api.ImaSdkFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends BrightcovePlayer {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    private EventEmitter eventEmitter;
+    private GoogleIMAComponent googleIMAComponent;
+    private BrightcoveMediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.activity_main);
+
+        brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
+        mediaController = new BrightcoveMediaController(brightcoveVideoView);
+        brightcoveVideoView.setMediaController(mediaController);
+
+        super.onCreate(savedInstanceState);
+
+        eventEmitter = brightcoveVideoView.getEventEmitter();
+        setupGoogleIMA();
+
+        Map<String, String> options = new HashMap<String, String>();
+        List<String> values = new ArrayList<String>(Arrays.asList(VideoFields.DEFAULT_FIELDS));
+        values.remove(VideoFields.HLS_URL);
+        options.put("video_fields", StringUtil.join(values, ","));
+
+        Catalog catalog = new Catalog(eventEmitter, getString(R.string.account_id), getString(R.string.policy_key));
+//        catalog.findVideoByID("5714150331001", new VideoListener() {
+//            @Override
+//            public void onVideo(Video video) {
+//                brightcoveVideoView.add(video);
+//                brightcoveVideoView.start();
+//            }
+//        });
+        catalog.findPlaylistByReferenceID("play_2017_4_videos", new PlaylistListener() {
+            public void onPlaylist(Playlist playlist) {
+                brightcoveVideoView.addAll(playlist.getVideos());
+            }
+
+            public void onError(String error) {
+                Log.e(TAG, error);
+            }
+        });
+    }
+
+    private String[] googleAds = {
+            "http://pubads.g.doubleclick.net/gampad/ads?sz=400x300&iu=%2F6062%2Fhanna_MA_group%2Fvideo_comp_app&ciu_szs=&impl=s&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&m_ast=vast&url=[referrer_url]&correlator=[timestamp]"
+//           "https://pubads.g.doubleclick.net/gampad/ads?sz=1280x720&iu=/18689016/setn_video_pre-roll_1280x720_2&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]"
+    };
+
+    private void setupCuePoints(Source source) {
+        String cuePointType = "ad";
+        Map<String, Object> properties = new HashMap<String, Object>();
+        Map<String, Object> details = new HashMap<String, Object>();
+
+        CuePoint cuePoint = new CuePoint(CuePoint.PositionType.BEFORE, cuePointType, properties);
+        details.put(Event.CUE_POINT, cuePoint);
+        eventEmitter.emit(EventType.SET_CUE_POINT, details);
+
+        if (!source.getDeliveryType().equals(DeliveryType.HLS)) {
+            int cuepointTime = 10 * (int) DateUtils.SECOND_IN_MILLIS;
+            cuePoint = new CuePoint(cuepointTime, cuePointType, properties);
+            details.put(Event.CUE_POINT, cuePoint);
+            eventEmitter.emit(EventType.SET_CUE_POINT, details);
+            mediaController.getBrightcoveSeekBar().addMarker(cuepointTime);
+        }
+
+        cuePoint = new CuePoint(CuePoint.PositionType.AFTER, cuePointType, properties);
+        details.put(Event.CUE_POINT, cuePoint);
+        eventEmitter.emit(EventType.SET_CUE_POINT, details);
+    }
+
+    private void setupGoogleIMA() {
+
+        eventEmitter.on(EventType.DID_SET_SOURCE, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                setupCuePoints((Source) event.properties.get(Event.SOURCE));
+            }
+        });
+
+        final ImaSdkFactory sdkFactory = ImaSdkFactory.getInstance();
+
+        eventEmitter.on(EventType.AD_STARTED, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                Log.v(TAG, event.getType());
+            }
+        });
+
+        eventEmitter.on(GoogleIMAEventType.DID_FAIL_TO_PLAY_AD, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                Log.v(TAG, event.getType());
+            }
+        });
+
+        eventEmitter.on(EventType.AD_COMPLETED, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                Log.v(TAG, event.getType());
+            }
+        });
+
+        eventEmitter.on(GoogleIMAEventType.ADS_REQUEST_FOR_VIDEO, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                AdDisplayContainer container = sdkFactory.createAdDisplayContainer();
+                container.setPlayer(googleIMAComponent.getVideoAdPlayer());
+                container.setAdContainer(brightcoveVideoView);
+
+                ArrayList<CompanionAdSlot> companionAdSlots = new ArrayList<CompanionAdSlot>();
+                CompanionAdSlot companionAdSlot = sdkFactory.createCompanionAdSlot();
+                ViewGroup adFrame = (ViewGroup) findViewById(R.id.brightcove_video_view);
+                companionAdSlot.setContainer(adFrame);
+                companionAdSlot.setSize(adFrame.getWidth(), adFrame.getHeight());
+                companionAdSlots.add(companionAdSlot);
+                container.setCompanionSlots(companionAdSlots);
+
+
+                ArrayList<AdsRequest> adsRequests = new ArrayList<AdsRequest>(googleAds.length);
+                for (String adURL : googleAds) {
+                    AdsRequest adsRequest = sdkFactory.createAdsRequest();
+                    adsRequest.setAdTagUrl(adURL);
+                    adsRequest.setAdDisplayContainer(container);
+                    adsRequests.add(adsRequest);
+                }
+
+                event.properties.put(GoogleIMAComponent.ADS_REQUESTS, adsRequests);
+                eventEmitter.respond(event);
+            }
+        });
+
+        googleIMAComponent = new GoogleIMAComponent(brightcoveVideoView, eventEmitter);
+    }
+
+
+
+}

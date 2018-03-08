a8cb4fd1cc0a67caafc62d53217dbb3a2c0ff1d4
indianstau <indianstau@yahoo.com.tw>
2018-03-08 10:06:27 +0800

    Update MainActivity.java

--- Android/Live/MainActivity.java
+++ Android/Live/MainActivity.java
@@ -1,6 +1,5 @@
 package com.brightcove.live;
 
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.brightcove.player.media.DeliveryType;
@@ -19,8 +18,8 @@ public class MainActivity extends BrightcovePlayer {
         brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
         super.onCreate(savedInstanceState);
 
-        Video video = Video.createVideo("https://tvbs-alive1.akamaized.net/c3f920d0a52946c287327174357a4fb1/ap-southeast-1/4862438529001/playlist.m3u8?hdnts=exp=1507617642~acl=/*~hmac=ca39204cb58c8ce5572e4eaf4a882b523c38042ad7db76f64a30a432d9ce3f41", DeliveryType.HLS);
-        video.getProperties().put(Video.Fields.PUBLISHER_ID, "4862438529001");
+        Video video = Video.createVideo("HLS_URL", DeliveryType.HLS);
+        video.getProperties().put(Video.Fields.PUBLISHER_ID, "accound_id");
         brightcoveVideoView.add(video);
         brightcoveVideoView.start();
     }

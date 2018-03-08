1c867a7f8374f7b93c353231707dd7bc457a3879
indianstau <indianstau@yahoo.com.tw>
2018-03-08 10:27:04 +0800

    Video

---
+++ Android/Video/MainActivity.java
@@ -0,0 +1,39 @@
+package com.brightcove.samplevide2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.EventEmitter;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.brightcove.player.view.BrightcovePlayer;
+
+public class MainActivity extends BrightcovePlayer {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
+        super.onCreate(savedInstanceState);
+
+        EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
+        Catalog catalog = new Catalog(eventEmitter, getString(R.string.account), getString(R.string.policy));
+
+        catalog.findVideoByID(getString(R.string.videoId), new VideoListener() {
+
+            // Add the video found to the queue with add().
+            // Start playback of the video with start().
+            @Override
+            public void onVideo(Video video) {
+                Log.v(TAG, "onVideo: video = " + video);
+                brightcoveVideoView.add(video);
+                brightcoveVideoView.start();
+            }
+        });
+    }
+}
+//https://github.com/BrightcoveOS/android-player-samples/blob/master/brightcove-exoplayer/BasicSampleApp/src/main/java/com/brightcove/player/samples/exoplayer/basic/MainActivity.java
